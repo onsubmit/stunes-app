d7ffd0a1ba0c17f47a38cdd85eaa56fdfc68b02f
Andy <onsubmit@outlook.com>
2023-05-06 11:24:29 -0700

    Delete jest files

--- src/server/.jest/setEnvVars.ts
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV !== 'test') {
-  throw new Error(`This file should only be imported by Jest. NODE_ENV='${process.env.NODE_ENV}'`);
-}
-
-process.env.CLIENT_ID = 'test-client-id';
-process.env.CLIENT_SECRET = 'test-client-secret';
-process.env.REDIRECT_URI = 'http://localhost:5001/callback';
