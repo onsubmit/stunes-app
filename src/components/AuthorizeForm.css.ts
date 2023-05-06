cdb1f6446f3deacbbade6871bbcf6f05e190940a
Andy <onsubmit@outlook.com>
2023-05-06 11:48:49 -0700

    Show profile photo and display name

--- src/components/AuthorizeForm.css.ts
+++ src/components/AuthorizeForm.css.ts
@@ -1,3 +1,9 @@
 import { style } from 'typestyle';
 
 export const className = style({});
+
+export const profilePhoto = style({
+  width: '40px',
+  height: '40px',
+  borderRadius: '50%',
+});
--- src/components/AuthorizeForm.tsx
+++ src/components/AuthorizeForm.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
 import { Err, Ok, Result } from 'ts-results';
 
 import localStorageManager, { LocalStorage } from '../utils/LocalStorage';
-import { className } from './AuthorizeForm.css';
+import { className, profilePhoto } from './AuthorizeForm.css';
 
 function AuthorizeForm() {
+  const [displayName, setDisplayName] = useState('');
+  const [profilePhotoUrl, setProfilePhotoUrl] = useState('');
+
   const hash = window.location.hash;
 
   const getHashDataResult = getHashData();
@@ -61,7 +66,40 @@ function AuthorizeForm() {
     return Err.EMPTY;
   }
 
-  return <div className={className}>{showConnectButton && <a href="/login">Connect to Spotify</a>}</div>;
+  function getAccessToken(): string {
+    const accessTokenResult = localStorageManager.get<string>('access_token');
+    if (accessTokenResult.err) {
+      throw new Error('Failure to retrieve access_token');
+    }
+
+    return accessTokenResult.val;
+  }
+
+  if (!showConnectButton) {
+    getMeAsync();
+  }
+
+  async function getMeAsync() {
+    const spotifyApi = new SpotifyWebApi();
+    spotifyApi.setAccessToken(getAccessToken());
+
+    const profile = await spotifyApi.getMe();
+    setDisplayName(profile.display_name || 'unknown username');
+    setProfilePhotoUrl((profile.images && profile.images[0].url) || '');
+  }
+
+  return (
+    <div className={className}>
+      {showConnectButton ? (
+        <a href="/login">Connect to Spotify</a>
+      ) : (
+        <>
+          <img className={profilePhoto} src={profilePhotoUrl}></img>
+          <div>{displayName}</div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default AuthorizeForm;
