278d0a275deea7c364ec7b824ff4b48d5cfdb858
Andy <onsubmit@outlook.com>
2024-07-28 16:16:00 -0700

    Rename test setup script

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    setupFiles: './src/server/.jest/setEnvVars.ts',
+    setupFiles: './src/server/.vitest/setEnvVars.ts',
   },
 });
